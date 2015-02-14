d23809787e869e334250abfdc171640cc14c1d57
alenoosh <hope2end@gmail.com>
2015-02-14 11:11:31 +0330

    A bug in actions.blade.php (due to missing conflict) is fixed.

--- src/views/datagrid/actions.blade.php
+++ src/views/datagrid/actions.blade.php
@@ -1,15 +1,4 @@
-
-
 @if (in_array("show", $actions))
-<<<<<<< HEAD
-    <a class="" title="@lang('rapyd::rapyd.show')" href="{{ $uri }}?show={{ $id }}"><span class="ic-globe"> </span></a>
-@endif
-@if (in_array("modify", $actions))
-    <a class="" title="@lang('rapyd::rapyd.modify')" href="{{ $uri }}?modify={{ $id }}"><span class="fa fa-edit"> </span></a>
-@endif
-@if (in_array("delete", $actions))
-    <a class="text-danger" title="@lang('rapyd::rapyd.delete')" href="{{ $uri }}?delete={{ $id }}"><span class="glyphicon glyphicon-trash"> </span></a>
-=======
     <a class="" title="@lang('rapyd::rapyd.show')" href="{!! $uri !!}?show={!! $id !!}"><span class="glyphicon glyphicon-eye-open"> </span></a>
 @endif
 @if (in_array("modify", $actions))
@@ -17,5 +6,4 @@
 @endif
 @if (in_array("delete", $actions))
     <a class="text-danger" title="@lang('rapyd::rapyd.delete')" href="{!! $uri !!}?delete={!! $id !!}"><span class="glyphicon glyphicon-trash"> </span></a>
->>>>>>> upstream/2.0
 @endif
