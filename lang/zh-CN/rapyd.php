82ba9f9eda3805c4845d6ef9dbcce0bdfc27ffd9
alenoosh <hope2end@gmail.com>
2015-08-31 12:15:36 +0430

    Some translations are added.

--- lang/zh-CN/rapyd.php
+++ lang/zh-CN/rapyd.php
@@ -19,5 +19,7 @@ return array(
     'err_unknown'=> '错误，你没有选中任何数据。',
     'err_dup_pk' => '错误，数据主键重复。',
     'conf_delete'=> '你确定要删除这行数据?',
-
+    'empty_list' => 'No record is added !',
+    'help'       => 'Help',
+    'links_help' => "The 'Edit Links' form is used for adding a link in the sidebar menu of panel. The value of 'Display' field specifies the label of the link to be displayed in the menu and the value of 'Link' field specifies the name of the model."
 );
