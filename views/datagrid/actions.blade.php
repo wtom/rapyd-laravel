ea0c4efe2c9ba24720933f74089201902e6d56d8
Johannes Kees <johannes.kees@department-m.com>
2017-01-18 16:24:42 +0100

    Added 2 actions

--- views/datagrid/actions.blade.php
+++ views/datagrid/actions.blade.php
@@ -4,6 +4,12 @@
 @if (in_array("modify", $actions))
     <a class="" title="@lang('rapyd::rapyd.modify')" href="{!! url('panel/'.$current_entity.'/'.$uri) !!}?modify={!! $id !!}"><span class="fa fa-edit"> </span></a>
 @endif
+@if (in_array("codes", $actions))
+    <a class="" title="@lang('Codes')" href="{!! url('panel/'.$current_entity.'/'.$uri) !!}?codes={!! $id !!}"><i class="fa fa-list-ol"> </i></a>
+@endif
+@if (in_array("generate", $actions))
+    <a class="" title="@lang('Generate')" href="{!! url('panel/'.$current_entity.'/'.$uri) !!}?generate={!! $id !!}"><i class="fa fa-file-text"> </i></a>
+@endif
 @if (in_array("delete", $actions))
     <a class="text-danger" title="@lang('rapyd::rapyd.delete')" href="{!! url('panel/'.$current_entity.'/'.$uri) !!}?delete={!! $id !!}"><span class="glyphicon glyphicon-trash"> </span></a>
 @endif
