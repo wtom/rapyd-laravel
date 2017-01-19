17f47e60bbacb83c9efc45e9141b1876bd99a73c
Johannes Kees <johannes.kees@department-m.com>
2017-01-19 16:21:02 +0100

    Changed voucher url

--- views/datagrid/actions.blade.php
+++ views/datagrid/actions.blade.php
@@ -5,7 +5,7 @@
     <a class="" title="@lang('rapyd::rapyd.modify')" href="{!! url('panel/'.$current_entity.'/'.$uri) !!}?modify={!! $id !!}"><span class="fa fa-edit"> </span></a>
 @endif
 @if (in_array("codes", $actions))
-    <a class="" title="@lang('Codes')" href="{!! url('panel/'.$current_entity.'/'.$uri) !!}?codes={!! $id !!}"><i class="fa fa-list-ol"> </i></a>
+    <a class="" title="@lang('Codes')" href="{!! url('panel/Voucher/all') !!}?campaign={!! $id !!}"><i class="fa fa-list-ol"> </i></a>
 @endif
 @if (in_array("generate", $actions))
     <a class="" title="@lang('Generate')" href="{!! url('panel/'.$current_entity.'/'.$uri) !!}?generate={!! $id !!}"><i class="fa fa-file-text"> </i></a>
